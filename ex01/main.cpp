47ab5983419136994a4420fdb74284f8414cc7fb
Eliott Vallee dore <evallee-@c1r5p4.42quebec.com>
2023-12-07 14:59:49 -0500

    cpp04 done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,27 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: niceguy <niceguy@student.42.fr>            +#+  +:+       +#+        */
+/*   By: evallee- <evallee-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 17:51:53 by evallee-          #+#    #+#             */
-/*   Updated: 2023/12/06 21:11:36 by niceguy          ###   ########.fr       */
+/*   Updated: 2023/12/07 14:58:53 by evallee-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <memory>
 #include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
 
 int	main(void)
 {
-	std::auto_ptr<Animal> array[20];
-	int	i;
+	AAnimal*	array[20];
+	int			i;
 
 	for (i = 0; i < 10; i++)
-		array[i].reset(new Dog());
+		array[i] = new Dog();
 	for (i = 10; i < 20; i++)
-		array[i].reset(new Cat());
+		array[i] = new Cat();
 	
 	for (i = 0; i < 20; i++)
 	{
@@ -32,4 +31,10 @@ int	main(void)
 		std::cout << std::endl;
 	}
 	std::cout << std::endl;
+
+	for (i = 0; i < 20; i++)
+	{
+		delete array[i];
+		array[i] = NULL;
+	}
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,17 +6,16 @@
 /*   By: evallee- <evallee-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 17:51:53 by evallee-          #+#    #+#             */
-/*   Updated: 2023/12/07 14:24:11 by evallee-         ###   ########.fr       */
+/*   Updated: 2023/12/07 14:59:11 by evallee-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <memory>
 #include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
 
 int	main(void)
-{
+{	
 	AAnimal*	array[20];
 	int			i;
 
@@ -34,5 +33,8 @@ int	main(void)
 	std::cout << std::endl;
 
 	for (i = 0; i < 20; i++)
+	{
 		delete array[i];
+		array[i] = NULL;
+	}
 }
